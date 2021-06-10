138ae619d3e006d8bf3438f5d4fd25fe1e2bd0e1
Giacomo Zanotti <giacomo.zanotti@kalpa.it>
2021-06-10 16:29:48 +0200

    added broadcast in PositionPublisher

--- src/main/java/com/oracolo/findmycar/mqtt/PositionPublisher.java
+++ src/main/java/com/oracolo/findmycar/mqtt/PositionPublisher.java
@@ -11,16 +11,19 @@ import org.slf4j.LoggerFactory;
 
 import com.oracolo.findmycar.mqtt.messages.PositionMessage;
 
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+
 @ApplicationScoped
 public class PositionPublisher {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-
 	@Inject
 	@Channel("positions")
+	@Broadcast
 	Emitter<PositionMessage> positionMessageEmitter;
-	public void sendPosition(PositionMessage positionMessage){
-		logger.debug("Sending position message {}",positionMessage);
+
+	public void sendPosition(PositionMessage positionMessage) {
+		logger.debug("Sending position message {}", positionMessage);
 		positionMessageEmitter.send(Message.of(positionMessage));
 	}
 }
